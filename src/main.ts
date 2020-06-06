141019cb5c884ce70ccf34256036ca825cf4162c
Austin <12504656+Austin-Scott@users.noreply.github.com>
2020-06-06 11:16:39 -0500

    minimum Discord starting bot

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello World')
+import Discord from 'discord.js'
+import fs from 'fs'
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}`)
+})
+
+client.on('message', msg => {
+    if(msg.content == 'ping') {
+        msg.reply('Pong!')
+    }
+})
+
+client.login(JSON.parse(fs.readFileSync('token.json', { encoding: 'utf-8' })).token)
